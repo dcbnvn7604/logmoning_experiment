ec6e4b0ce8863d5bfa9de87eee2eeca3965df502
HoaTV <hoatv@gemvietnam.com>
2023-04-07 14:28:34 +0700

    add: expressjs healthcheck

--- expressjs/app.js
+++ expressjs/app.js
@@ -2,14 +2,18 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combined'))
+app.use(morgan('combined', {
+    skip: (req, res) => {
+        return req.baseUrl != '/healcheck'
+    }
+}))
 
 const port = 80
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
+app.get('/healcheck', (req, res) => {
+    res.send('server healthy')
 })
 
 app.listen(port, () => {
-    console.info('App running')
+    console.info('server running')
 })
