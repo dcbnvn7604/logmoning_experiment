6964b4c6dae8ec972d3d0386f47e14f70cfd9d73
HoaTV <hoatv@gemvietnam.com>
2023-04-11 10:01:34 +0700

    add: expressjs access log to file

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const morgan = require('morgan')
+const rfs = require("rotating-file-stream")
+
+const accessLogStream = rfs.createStream("access.log", {
+    size: "10M",
+    interval: "1d",
+    maxFiles: 3,
+    path: '/var/log/expressjs'
+})
 
 const app = express()
 app.use(morgan('combined', {
     skip: (req, res) => {
         return req.originalUrl == '/healcheck'
-    }
+    },
+    stream: accessLogStream
 }))
 
 const port = 80
