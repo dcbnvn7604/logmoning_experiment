965ca750ab0bfa74b7cd6832927b287543dfd8af
HoaTV <hoatv@gemvietnam.com>
2023-04-11 14:36:37 +0700

    add: expressjs requestId in log

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const rfs = require("rotating-file-stream")
-const winston = require('winston');
-const { combine, timestamp, json } = winston.format;
+const winston = require('winston')
+const { AsyncLocalStorage } = require('async_hooks')
+const { v4: uuid } = require("uuid");
+const { combine, timestamp, json } = winston.format
 
 LOG_DIR = '/var/log/expressjs'
 
@@ -20,22 +22,51 @@ const appLogStream = rfs.createStream("app.log", {
     path: LOG_DIR
 })
 
-const logger = winston.createLogger({
+const winstonLogger = winston.createLogger({
     level: 'debug',
     format: combine(timestamp(), json()),
     defaultMeta: { service: 'expressjs' },
     transports: [new winston.transports.Stream({
         stream: appLogStream
     })],
-  });
+});
+
+const logger = {
+    debug: (info) => {
+        store = context.getStore()
+        if (store && store.get('requestId')) {
+            winstonLogger.debug(info, {'requestId': store.get('requestId')})
+        } else {
+            winstonLogger.debug(info)
+        }
+    },
+    info: (info) => {
+        store = context.getStore()
+        if (store && store.get('requestId')) {
+            winstonLogger.info(info, {'requestId': store.get('requestId')})
+        } else {
+            winstonLogger.info(info)
+        }
+    }
+}
 
 const app = express()
+
 app.use(morgan('combined', {
     skip: (req, res) => {
         return req.originalUrl == '/healcheck'
     },
     stream: accessLogStream
 }))
+
+const context = new AsyncLocalStorage();
+app.use((req, res, next) => {
+    const store = new Map();
+    context.run(store, () => {
+        store.set('requestId', uuid())
+        next()
+    });
+})
 
 const port = 80
 
