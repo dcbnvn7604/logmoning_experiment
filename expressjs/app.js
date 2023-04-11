5d496e6fa57018f54726da0cb1eec2cf22e8b24c
HoaTV <hoatv@gemvietnam.com>
2023-04-11 11:47:49 +0700

    add: expressjs winston

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const morgan = require('morgan')
 const rfs = require("rotating-file-stream")
+const winston = require('winston');
+const { combine, timestamp, json } = winston.format;
+
+LOG_DIR = '/var/log/expressjs'
 
 const accessLogStream = rfs.createStream("access.log", {
     size: "10M",
     interval: "1d",
     maxFiles: 3,
-    path: '/var/log/expressjs'
+    path: LOG_DIR
 })
+
+const appLogStream = rfs.createStream("app.log", {
+    size: "10M",
+    interval: "1d",
+    maxFiles: 3,
+    path: LOG_DIR
+})
+
+const logger = winston.createLogger({
+    level: 'debug',
+    format: combine(timestamp(), json()),
+    defaultMeta: { service: 'expressjs' },
+    transports: [new winston.transports.Stream({
+        stream: appLogStream
+    })],
+  });
 
 const app = express()
 app.use(morgan('combined', {
@@ -24,9 +44,10 @@ app.get('/healcheck', (req, res) => {
 })
 
 app.get('/api/chain', (req, res) => {
+    logger.debug({data: 'data debug'})
     res.send('success')
 })
 
 app.listen(port, () => {
-    console.info('server running')
+    logger.info('server running')
 })
